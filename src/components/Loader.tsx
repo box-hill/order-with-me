daf8fcce9bf135a0785b27d1862989a78199eefa
aaron <aaronlim314@gmail.com>
2021-11-03 10:23:29 +1100

    Added loader styling

---
+++ src/components/Loader.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Loader(){
+    return (
+        <div className="card">
+            <div className="loader">
+                <div className="spin"></div>
+		        <div className="bounce"></div>
+            </div>
+        </div>
+    );
+}
+
+export default Loader;
