b7af9f3082ab5ad52c4e5a8a6c51ce22f50e8d92
aaron <aaronlim314@gmail.com>
2021-11-08 13:37:15 +1100

    Added key to list of categories

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -11,8 +11,8 @@ function Menu() {
         <div className='menu-grid'>
             {categories.map((category: Category, index: number) => {
                 return (
-                    <div className='grid-item' style={{backgroundImage: `linear-gradient(to right, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(${category.imgUrl})`}}>
-                        <Link key={index} to={`/menu/${category.name.toLowerCase()}`}>
+                    <div key={index} className='grid-item' style={{backgroundImage: `linear-gradient(to right, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(${category.imgUrl})`}}>
+                        <Link to={`/menu/${category.name.toLowerCase()}`}>
                             {category.name}
                         </Link>
                     </div>
