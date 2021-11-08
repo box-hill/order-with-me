8e4d82424325b87a52ee07207b3c3cd081f281aa
aaron <aaronlim314@gmail.com>
2021-11-08 12:57:59 +1100

    Added background images and styles

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,15 +1,21 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import  { categories, Category } from "./categories";
+import { DiagnosticCategory } from 'typescript';
+import  { Category } from "./categories";
+const { categories } = require("./categories");
 
 
 function Menu() {
 
     return (
-        <div className='menu'>
-            {categories.map((category: Category, index) => {
+        <div className='menu-grid'>
+            {categories.map((category: Category, index: number) => {
                 return (
-                    <Link key={index} to={`/menu/${category.name.toLowerCase()}`}>{category.name}</Link>
+                    <div className='grid-item' style={{backgroundImage: `linear-gradient(to right, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(${category.imgUrl})`}}>
+                        <Link key={index} to={`/menu/${category.name.toLowerCase()}`}>
+                            {category.name}
+                        </Link>
+                    </div>
                 );
             })}
         </div>
--- src/components/categories.ts
+++ src/components/categories.ts
@@ -8,23 +8,23 @@ export interface Category {
 const categories: Category[] = [
     {
         name: "Meat",
-        imgUrl: "#",
+        imgUrl: "./img/meat.jpg",
     },
     {
         name: "Soup",
-        imgUrl: "#",
+        imgUrl: "https://images.unsplash.com/photo-1611345157614-26d3bdd10c93?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
     },
     {
         name: "Sides",
-        imgUrl: "#",
+        imgUrl: "./img/sides.jpg",
     },
     {
         name: "Dessert",
-        imgUrl: "#",
+        imgUrl: "./img/dessert.jpeg",
     },
     {
         name: "Drinks",
-        imgUrl: "#",
+        imgUrl: "https://images.unsplash.com/photo-1513558161293-cdaf765ed2fd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80",
     },
 ]
 
